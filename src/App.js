13bdc1fdd26c0135266397347899f2f50c955d5d
MattB09 <mattbancroft03@gmail.com>
2021-03-30 18:15:57 +0900

    Added myTaps state.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Map from './components/Map';
 import List from './components/List';
 
 function App() {
+  const [myTaps, setMyTaps] = useState([]);
+
+  useEffect(()=> {
+    console.log("tap list", myTaps)
+  }, [myTaps]);
+
   return (
     <div className="App">
       <Navbar />
-      <Map />
+      <Map setMyTaps={setMyTaps} myTaps={myTaps}/>
       <List />
 
     </div>
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 export default function ListItem(props) {
-    console.log(props.spot)
     return (
         <div>
             <p>{`${props.spot.name} ${props.spot.address}`}</p>
