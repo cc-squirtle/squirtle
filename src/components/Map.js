d041c72fcf8f995957f654924428196215dd4891
Matt Bancroft <mattbancroft03@gmail.com>
2021-03-30 15:55:51 +0900

    Added Map (#12)
    * Created Map shell but it's not working yet.
    
    * Made map appear.

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from 'react';
+import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+
+const dataTable = [
+    {
+        key: 368,
+        position: {
+            lat: 35.65276408536717, 
+            lng: 139.71233546727083
+        }
+    },
+    {
+        key: 225,
+        position: {
+            lat: 34.97158036354114, 
+            lng: 138.95753531309717
+        }
+    },
+    {
+        key: 381,
+        position: {
+            lat: 35.44024378020946,  
+            lng: 139.62879999071512
+        }
+    }
+]
+
+const MyMap = withGoogleMap((props) => (
+    <GoogleMap
+        defaultZoom={5}
+        defaultCenter={{ lat: 38.0947841436263, lng: 139.86262510034172 }} 
+    >
+        {dataTable.map((marker) => (
+            <Marker
+                key={marker.key}
+                {...marker}
+                onRightClick={() => console.log("marker was clicked")}
+            />
+        ))}
+    </GoogleMap>
+));
 
 export default function Map() {
     return (
-        <div>
-            
-        </div>
+        <MyMap
+            containerElement={<div style={{ height: `500px`, width: `900px` }}/>}
+            mapElement={<div style={{ height: `100%` }} />}
+            onMapLoad={()=> console.log('map is loaded')}
+        />
     )
 }
