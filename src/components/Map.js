27acc370981e60868fe70fec1a1068791189f517
MattB09 <mattbancroft03@gmail.com>
2021-03-30 16:39:30 +0900

    Separated styling from map component.

--- src/components/Map.js
+++ src/components/Map.js
@@ -42,10 +42,11 @@ const MyMap = withGoogleMap((props) => (
 
 export default function Map() {
     return (
-        <MyMap
-            containerElement={<div style={{ height: `500px`, width: `900px` }}/>}
-            mapElement={<div style={{ height: `100%` }} />}
-            onMapLoad={()=> console.log('map is loaded')}
-        />
+            <MyMap
+                className="map"
+                containerElement={<div className="map-container" />}
+                mapElement={<div className="map" />}
+                onMapLoad={()=> console.log('map is loaded')}
+            />
     )
 }
