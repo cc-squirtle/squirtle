62af3b497398a4be98a07dd22c5e9ac368c49a8e
Maya Shinagawa <67887298+flying-coder@users.noreply.github.com>
2021-03-30 15:48:03 +0900

    skeleton of navbar added/logo file added (#13)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import logo from '../mymizu_logo.png'
 
 export default function Navbar() {
     return (
         <div>
-            Navbar
+            <img className = "logo" src = {logo}/>
+            <input type = "text"/>
+            <button type = "button">SEARCH</button>
         </div>
     )
 }
+
+
