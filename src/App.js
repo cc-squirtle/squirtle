c8e64c91e4864af4fc0da72483f238fdb4715af2
Matt Bancroft <mattbancroft03@gmail.com>
2021-03-31 09:29:27 +0900

    Feat/my taps (#23)
    * Created Map shell but it's not working yet.
    
    * Made map appear.
    
    * Separated styling from map component.
    
    * Added myTaps state.
    
    * Finished myTaps useState

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Map from './components/Map';
 import List from './components/List';
 
 function App() {
+  const [myTaps, setMyTaps] = useState([]);
+
+  useEffect(()=> {
+    console.log("tap list", myTaps)
+  }, [myTaps]);
+
   return (
     <div className="App">
       <Navbar />
-      <Map />
-      <List />
-
+      <Map setMyTaps={setMyTaps} />
+      <List myTaps={myTaps} />
     </div>
   );
 }
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,11 +1,10 @@
 import React from 'react'
 
 export default function ListItem(props) {
-    console.log(props.spot)
     return (
-        <div>
-            <p>{`${props.spot.name} ${props.spot.address}`}</p>
-            <img src={props.spot.pic}></img>
+        <div key={props.tap.id}>
+            <p>{`${props.tap.name} ${props.tap.address}`}</p>
+            <img src={props.tap.photo_url} alt="Image of tap"></img>
         </div>
     )
 }
