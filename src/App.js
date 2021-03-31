212b87abe785652c35c6780205c5032e2e67a4dc
flying-coder <mayashinagawa@gmail.com>
2021-03-31 15:12:03 +0900

    add Function Of Inputbar And Button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
+import React, { useState, useEffect } from 'react'
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar　from './components/Navbar';
 import Map from './components/Map';
 import List from './components/List';
 
 function App() {
+
+  const [center, setCenter] = useState("日本");
+
+  useEffect(()=> {
+    console.log("New center ", center);
+  }, [center])
+
   return (
     <div className="App">
-      <Navbar />
-      <Map />
+      <Navbar setCenter = { setCenter } />
+      <Map center={ center }/>
       <List />
-
     </div>
   );
 }
