2975ff30f762e1062c02abcd10887d1ada7dfb35
Jun Gao <junworks7@gmail.com>
2021-03-31 16:40:48 +0900

    fix: amend api of geo

--- services/server/routes.js
+++ services/server/routes.js
@@ -5,52 +5,19 @@ const axios = require('axios');
 
 // FOR GEO / Pass axios request by query param
 routes.get('/geo', async (req, res) => {
-  console.log('req.query.url', req.query.url, 'req.query.key', req.query.key);
+  console.log('req.query.str', req.query.str);
 
-  if (req.query.url === undefined || req.query.key === undefined) {
-    console.error('Url or key missing!', err);
+  if (req.query.str === undefined) {
+    console.error('str missing!', err);
     res.sendStatus(400);
   }
 
+  const key = 'AIzaSyBkQ6o2TtjqGIL3TlzOgJaKBwcw3MNDcJQ';
   let config = {
     method: 'get',
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
-      req.query.url
-    )}&key=${req.query.key}`,
-  };
-
-  const result = await axios(config);
-  console.log(result.data);
-  res.json(result.data);
-});
-
-// FOR MyMizu / Pass axios request by query param
-routes.get('/mymizu', async (req, res) => {
-  console.log(
-    'mymizu api called. req.query.c:',
-    req.query.c1,
-    req.query.c2,
-    req.query.c3,
-    req.query.c4
-  );
-
-  if (
-    req.query.c1 === undefined ||
-    req.query.c2 === undefined ||
-    req.query.c3 === undefined ||
-    req.query.c4 === undefined
-  ) {
-    console.error('some c missing!', err);
-    res.sendStatus(400);
-  }
-
-  let config = {
-    method: 'get',
-    url: `https://my-mizu-dev2-gen8n.ondigitalocean.app/dev-api/search/area?c1=${req.query.c1}&c2=${req.query.c2}&c3=${req.query.c3}&c4=${req.query.c4}`,
-    headers: {
-      Authorization: 'Bearer 1|qRCvaTxpPdSaD7JS5UDLY1EOjwYn9du9aqaa8gaW',
-      Cookie: '__cfduid=d0ed2ee922756fedb09c99176b1d040261617159177',
-    },
+      req.query.str
+    )}&key=${key}`,
   };
 
   const result = await axios(config);
