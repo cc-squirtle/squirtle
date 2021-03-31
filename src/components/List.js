d137c8307fa85313894e393cd26805488361860d
Matt Bancroft <mattbancroft03@gmail.com>
2021-03-31 12:13:13 +0900

    Feat/database (#25)
    * Created Map shell but it's not working yet.
    
    * Made map appear.
    
    * Separated styling from map component.
    
    * Added myTaps state.
    
    * Finished myTaps useState
    
    * Changed package.json scripts. Added services folder with db and server files.
    
    * Added save logic to List. Added alt attributes to all images.

--- src/components/List.js
+++ src/components/List.js
@@ -1,31 +1,37 @@
 import React from 'react'
 import ListItem from './ListItem'
 
-const dummy = [{
-    name: "codechrysalis",
-    address: "B2 Vort, ３丁目-１-３５ 元麻布 港区 東京都 106-0046",
-    pic: "https://assets.st-note.com/production/uploads/images/15772814/rectangle_large_type_2_a54e42b9080f72407da8c309fe30d041.jpeg?width=800"
-},
-{
-    name: "codechrysalis2",
-    address: "B2 Vort, ３丁目-１-３５ 元麻布 港区 東京都 106-0046",
-    pic: "https://assets.st-note.com/production/uploads/images/15772814/rectangle_large_type_2_a54e42b9080f72407da8c309fe30d041.jpeg?width=800"
-},
-{
-    name: "codechrysalis3",
-    address: "B2 Vort, ３丁目-１-３５ 元麻布 港区 東京都 106-0046",
-    pic: "https://assets.st-note.com/production/uploads/images/15772814/rectangle_large_type_2_a54e42b9080f72407da8c309fe30d041.jpeg?width=800"
-}
-]
-
 export default function List(props) {
+
+    function handleSave() {
+        if (!props.myTaps.length){
+            alert("nothing to save");
+        }
+
+        // API call to delete current list?
+        let saveList = props.myTaps.map(tap => {
+            return {
+                mymizu_id: tap.id,
+                name: tap.name,
+                longitude: tap.longitude,
+                latitude: tap.latitude,
+                address: tap.address,
+                comment: tap.comment,
+                photo_url: tap.photo_url
+            }
+        })
+        console.log(saveList);
+        // API call to insert list to db...
+    }
+
     return (
         <div>
             <h2>List</h2>
-            <button>
+            <button
+            onClick={handleSave}>
                 save
             </button>
-            {props.myTaps.length && props.myTaps.map((tap) => <ListItem tap={tap}/>)} 
+            {props.myTaps.length && props.myTaps.map((tap) => <ListItem tap={tap} />)} 
         </div>
     )
 }
